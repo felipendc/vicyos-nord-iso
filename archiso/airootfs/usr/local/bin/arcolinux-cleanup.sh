ab769009eeeb363b2b1edf28e0ad9dd67cae00f8
felipendc <felipendc10@gmail.com>
2020-08-31 09:15:47 -0300

    Vicyos-Nord Rolling Update: (seg 31 ago 2020 09:15:47 -03)

--- archiso/airootfs/usr/local/bin/arcolinux-cleanup.sh
+++ archiso/airootfs/usr/local/bin/arcolinux-cleanup.sh
@@ -5,7 +5,7 @@ rm -f /etc/polkit-1/rules.d/49-nopasswd_global.rules
 rm -r /etc/systemd/system/etc-pacman.d-gnupg.mount
 rm /root/{.automated_script.sh,.zlogin}
 rm /usr/local/bin/arcolinux-all-cores.sh
-mv /etc/arcolinux-release /etc/lsb-release
+mv /etc/vicyos-release /etc/lsb-release
 pacman -R mkinitcpio-archiso --noconfirm
 mv /etc/mkinitcpio.d/arcolinux /etc/mkinitcpio.d/linux.preset
 rm /usr/local/bin/arcolinux-cleanup.sh
